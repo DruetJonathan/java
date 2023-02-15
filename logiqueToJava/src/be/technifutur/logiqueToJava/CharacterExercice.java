b1759b1ec4a70f30ff7c429dce1f87744e56553c
Jonathan Druet <jonathandruet2@gmail.com>
2023-02-15 15:25:02 +0100

    Signed-off-by: Jonathan Druet <jonathandruet2@gmail.com>

--- logiqueToJava/src/be/technifutur/logiqueToJava/CharacterExercice.java
+++ logiqueToJava/src/be/technifutur/logiqueToJava/CharacterExercice.java
@@ -5,20 +5,24 @@ import java.util.Scanner;
 public class CharacterExercice implements Runnable {
     public void run() {
         Scanner scan = new Scanner(System.in);
-        boolean recherche = true;
-
+        int firstPos = 0, compteurNombreLettre = 0;
         System.out.print("Entrez un mot: ");
         String texte = scan.nextLine();
         System.out.print("Entrez une lettre à chercher: ");
         char lettre = scan.nextLine().charAt(0);
 
-        for (int i = 0; i < texte.length() && recherche; i++) {
+        for (int i = 0; i < texte.length(); i++) {
             if (texte.charAt(i) == lettre) {
-                recherche = false;
+                if (compteurNombreLettre == 0) {
+                    firstPos = i;
+                }
+                compteurNombreLettre++;
+
             }
         }
-        if (!recherche) {
-            System.out.println("Le caractère a été trouvé");
+        if (compteurNombreLettre != 0) {
+            System.out.println("Le caractère a été trouvé sa première position est " + firstPos + " et il est apparut "
+                    + compteurNombreLettre + " fois");
         } else {
             System.out.println("Le caractère n'a pas été trouvé");
         }
